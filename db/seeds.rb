d02764fd31d2c18a9fd98046e8dca6046321e49f
Cory D. Hall <corydantehall@gmail.com>
2015-09-21 21:31:10 -0400

    Adds option to seed databas from tumblr.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 class Seeder
-  def self.seed(query, subspace = "")
+  def self.seed_from_google(query, subspace = "")
     subspace += subspace.length > 0 ? "/" : ""
     agent = Mechanize.new
     agent.log = Logger.new "mech.log"
@@ -30,9 +30,48 @@ class Seeder
     Dir["seed_data/#{subspace}#{query}/*"]
   end
 
-  def self.add_from_query_to(user, *query)
+  def self.seed_from_tumblr(query, subspace = "")
+    subspace += subspace.length > 0 ? "/" : ""
+    agent = Mechanize.new
+    agent.log = Logger.new "mech.log"
+    agent.user_agent_alias = 'Mac Safari'
+    agent.pluggable_parser.default = Mechanize::FileSaver
+
+    page = agent.get "http://www.tumblr.com/"
+    search_form = page.form_with :id => "search_form"
+    search_form.field_with(:name => "q").value = query
+
+    search_results = agent.submit search_form
+
+    image_links = search_results.image_urls
+    p
+    image_links.each_with_index do |image_url, idx|
+      p image_url.to_s.sub(/_500./, "_1280.")
+      if (image_url.to_s[/\Adata:|\/p\?/].nil?)&& image_url.to_s.length < 256
+        p "-----"
+        begin
+          img = (agent.get image_url.to_s.sub(/_500./, "_1280."))
+          img.save!("seed_data/#{subspace}#{query}/#{idx}-#{img.filename}")
+        rescue
+        end
+      end
+    end
+    Dir["seed_data/#{subspace}#{query}/*"]
+  end
+
+  def self.add_from_google_to(user, *query)
     files = query.inject([]) do |accum, query|
-      accum.concat seed(query)
+      accum.concat seed_from_google(query)
+    end
+    files.shuffle.each do |file|
+      user.photos.create photo: File.open(file)
+    end
+    user
+  end
+
+  def self.add_from_tumblr_to(user, *query)
+    files = query.inject([]) do |accum, query|
+      accum.concat seed_from_tumblr(query)
     end
     files.shuffle.each do |file|
       user.photos.create photo: File.open(file)
@@ -48,7 +87,7 @@ User.create({
   full_name: "admin"
 })
 admin = User.find(1)
-Seeder.add_from_query_to admin, "George Stubbs", "Yayoi Kusama"
+Seeder.add_from_tumblr_to admin, "George Stubbs", "Yayoi Kusama"
 # (seed("George Stubbs") + seed("Yayoi Kusama")).shuffle.each do |file|
 #   admin.photos.create photo: File.open(file)
 # end
@@ -60,12 +99,15 @@ User.create({
   full_name: "Kim Kardashian West"
 })
 kim_k = User.find_by(username: "kimkardashian")
+kim_k.update({
+  profile_picture: kim_k.photos.to_a.sample
+});
 
 # seed("North West").each do |file|
 #   kim_k.photos.create! photo: File.open(file)
 # end
 
-Seeder.add_from_query_to kim_k, "North West", "Kim Kardashian Eating"
+Seeder.add_from_tumblr_to kim_k, "North West", "Kim Kardashian Eating"
 
 joins = %w(_ __ ___ - x)
 100.times do
